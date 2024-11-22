e6c8e386808339615ef3fc2743f54c540d4579b5
Maxim-LD <ayinde779@gmail.com>
2024-11-22 21:46:46 +0100

    for starters

---
+++ index.js
@@ -0,0 +1,35 @@
+
+
+document.getElementById('request-form').addEventListener('submit', async (event) => {
+    event.preventDefault()
+
+
+    const firstName = document.getElementById('fname').value
+    const lastName = document.getElementById('lname').value
+    const email = document.getElementById('email').value
+    const password = document.getElementById('pass').value
+    const designation = document.getElementById('role').value
+
+
+    
+    try {
+        const response = await fetch('http://localhost:8089/api/create_user', {
+            method: "POST",
+            headers: {"Content-Type": "application/json" },
+            body: JSON.stringify({ firstName, lastName, email, password, designation }),      
+      })
+        
+        const data = await response.json()
+
+        const responseElement = document.getElementById("response")
+
+        responseElement.textContent = data.message
+        responseElement.className = "response-message error";
+        responseElement.style.display = "block";
+        console.log("Response from backend:", data)
+
+    } catch (error) {
+        console.error("Error sending data:", error)
+        document.getElementById("response").textContent = "An error occurred."
+    }
+})
